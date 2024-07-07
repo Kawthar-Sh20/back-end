a64a7fac5cc4a26955d915bb0c2ce46cb8a9069c
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-07 16:03:17 +0300

    Test

--- backend.py
+++ backend.py
@@ -4,4 +4,4 @@ with open('../config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
 if config['run_localhost']:
-    print("Error: Cannot run on localhost.")
+    print("Error: Cannot run on localhost.")  
